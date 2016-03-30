b23dde9a7c9a18104a051d6c73bb6dfe7744a05c
Carlos <penguingamer.carlos@gmail.com>
2016-03-30 19:47:07 -0400

    Rewrote ClimberScorer Class
    Rewrote ClimberScorer Class to have left side and right side methods to
    match the new robot design,

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/robot/ClimberScorer.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/robot/ClimberScorer.java
@@ -5,29 +5,48 @@ import com.qualcomm.robotcore.hardware.*;
  * Created by Carlos on 12/18/2015.
  */
 public class ClimberScorer {
-    Servo climberServo;
+    Servo leftClimberServo;
+    double leftScoringPosition = 0.0;
+    double leftUpwardPosition = 0.5;
+    double leftInitialPosition = 1.0;
 
-    double scoringPosition = 0.0;
-    double upwardPosition = 0.5;
-    double initialPosition = 1.0;
+    Servo rightClimberServo;
+    double rightScoringPosition = 0.0;
+    double rightUpwardPosition = 0.5;
+    double rightInitialPosition = 1.0;
 
     public ClimberScorer(){}
 
     public void init(HardwareMap hardwareMap){
-        climberServo = hardwareMap.servo.get("climberServo");
-        climberServo.setPosition(initialPosition);
+        leftClimberServo = hardwareMap.servo.get("leftClimberServo");
+        leftClimberServo.setPosition(leftInitialPosition);
+
+        rightClimberServo = hardwareMap.servo.get("rightClimberServo");
+        rightClimberServo.setPosition(rightInitialPosition);
     }
 
-    public void score(){
-        climberServo.setPosition(scoringPosition);
+    public void scoreLeftSide(){
+        leftClimberServo.setPosition(leftScoringPosition);
     }
 
-    public void reset(){
-        climberServo.setPosition(initialPosition);
+    public void resetLeftSide(){
+        leftClimberServo.setPosition(leftInitialPosition);
     }
 
-    public void raise(){
-        climberServo.setPosition(upwardPosition);
+    public void raiseLeftSide(){
+        leftClimberServo.setPosition(leftUpwardPosition);
+    }
+
+    public void rightSide(){
+        rightClimberServo.setPosition(rightScoringPosition);
+    }
+
+    public void resetRightSide(){
+        rightClimberServo.setPosition(rightInitialPosition);
+    }
+
+    public void raiseRightSide(){
+        rightClimberServo.setPosition(rightUpwardPosition);
     }
 
 
