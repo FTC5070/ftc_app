e4e6777d1a224026c6c88cd85fe3a8e3162e74df
Carlos <penguingamer.carlos@gmail.com>
2016-03-30 19:53:39 -0400

    Rewrote Lift Class
    Rewrote Lift class to have one motor instead of two to match the world
    championship robot design,

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/robot/Lift.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/robot/Lift.java
@@ -5,64 +5,31 @@ import com.qualcomm.robotcore.hardware.*;
  * Created by Carlos on 11/12/2015.
  */
 public class Lift {
-    public DcMotor rightMotor;
-    public DcMotor leftMotor;
-
-    public Servo leftShifter;
-    double leftShifterHighGear =  0.93;
-    double leftShifterLowGear = 0.75;
-
-    public Servo rightShifter;
-    double rightShifterHighGear = 0.35;
-    double rightShifterLowGear = 0.50;
-
-    public boolean isShiftedHigh = true;
+    public DcMotor liftMotor;
 
     public int targetPosition = 0;
+    int error = 0;
     double KP = 0.05;
+    int ERROR_TOLERANCE = 5;
 
-    public Lift(){
-
-    }
+    public Lift(){    }
 
     public void init(HardwareMap hardwareMap) {
-        leftMotor = hardwareMap.dcMotor.get("leftLiftMotor");
-        rightMotor = hardwareMap.dcMotor.get("rightLiftMotor");
-
-
-        leftShifter = hardwareMap.servo.get("leftLiftServo");
-        rightShifter = hardwareMap.servo.get("rightLiftServo");
-
-
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
-
-        leftShifter.setPosition(leftShifterHighGear);
-        rightShifter.setPosition(rightShifterHighGear);
-    }
+        liftMotor = hardwareMap.dcMotor.get("liftMotor");
+     }
 
     public void setSpeed(double speed){
-        leftMotor.setPower(speed);
-        rightMotor.setPower(speed);
+        liftMotor.setPower(speed);
     }
 
     public void updatePosition(){
 
-        int error = targetPosition - (leftMotor.getCurrentPosition()+rightMotor.getCurrentPosition())/2;
+        if(Math.abs(error) > ERROR_TOLERANCE)
+            error = targetPosition - liftMotor.getCurrentPosition();
+        else
+            error = 0;
 
         this.setSpeed(error * KP);
-    }
-
-
-    public void setGear(String gear){
-
-        if(gear == "Low") {
-            leftShifter.setPosition(leftShifterLowGear);
-            rightShifter.setPosition(rightShifterLowGear);
-        }
-        else if(gear == "High"){
-            leftShifter.setPosition(leftShifterHighGear);
-            rightShifter.setPosition(rightShifterHighGear);
-        }
     }
 }
 
